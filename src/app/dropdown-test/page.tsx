581eb3f3439d0757d55cab912ca37ae75536ebd3
gilandre <gilandre.egnakou@gmail.com>
2025-05-19 21:31:29 +0000

    chore: suppression de la page de test dropdown

--- src/app/dropdown-test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { DropdownMenuDemo } from "@/components/test-dropdown"
-
-export default function DropdownTestPage() {
-  return (
-    <div className="container mx-auto py-10">
-      <DropdownMenuDemo />
-    </div>
-  )
-} 
